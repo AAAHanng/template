b0b3c222803e0bd40d4b0f662bb7e09212b63069
AAAhanng <1664674148@qq.com>
2024-10-29 14:47:17 +0800

    init: create new rules

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,35 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import {ConfigEnv, defineConfig, loadEnv, UserConfig} from 'vite'
+import {resolve} from "path";
+import vue from '@vitejs/plugin-vue';
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-})
+export default defineConfig(({mode }: ConfigEnv): UserConfig => {
+
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return {
+    define: {
+      __APP_ENV__: JSON.stringify(env.APP_ENV),
+    },
+    resolve: {
+      alias: {
+        "@": resolve(__dirname, "./src"),
+      }
+    },
+    base: env.BASE_URL || '/', // 设置基础路径
+    plugins: [vue()], // 插件数组
+    server: {
+      host: env.VITE_HOST || 'localhost',
+      port: Number(env.VITE_PORT) || 3000,
+    },
+    build: {
+      outDir: 'dist',
+    },
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: `@import "@/styles/var.scss";`
+        }
+      }
+    },
+  };
+});
